85cb4915a39333b91e37e45df696cceaf914759f
Avirup Ghosh <oviirup@gmail.com>
2025-02-15 15:06:28 +0530

    ♻️ refactor logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,20 @@
 import pi from 'picocolors';
 import { ZodError } from 'zod';
 
-function customLogger(first: string | Function | null, ...rest: string[]) {
-  rest = rest.map((msg) => pi.dim(`         ${msg}`));
-  typeof first === 'function'
+function customLogger(first: string | Function, ...rest: string[]) {
+  return typeof first === 'function'
     ? console.log(first('[SPRITE]'), ...rest)
-    : first === null
-      ? console.log(...rest)
-      : console.log('[SPRITE]', first, ...rest);
+    : console.log('[SPRITE]', first, ...rest);
 }
 
 export const logger = {
-  log: (first: string | Function | null, ...rest: string[]) => customLogger(first, ...rest),
+  log: (msg: string, ...rest: string[]) => customLogger(msg, ...rest),
   success: (...msg: string[]) => customLogger(pi.green, ...msg),
   warn: (...msg: string[]) => customLogger(pi.yellow, ...msg),
   error: (...msg: string[]) => customLogger(pi.red, ...msg),
   zodError: (errors: ZodError) => {
     for (const err of errors.errors) {
       customLogger(pi.red, err.message);
-      customLogger(null, pi.dim(err.path.join(' 🞄 ')));
     }
   },
 };
